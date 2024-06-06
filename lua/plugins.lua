b4dbe58a8abadee473269aff5357ad187b560e92
eman <emanabela4@gmail.com>
2024-06-06 02:03:25 +0200

    autocomplete, etc..

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -2,19 +2,19 @@
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    { 
+    {
         "rebelot/kanagawa.nvim",
         config = function()
             vim.cmd.colorscheme("kanagawa-dragon")
@@ -23,74 +23,255 @@ require("lazy").setup({
     {
         "nvim-treesitter/nvim-treesitter",
         config = function()
-           require("nvim-treesitter.configs").setup({
-               ensure_installed = {"c", "lua", "vim", "vimdoc", "query" },
+            require("nvim-treesitter.configs").setup({
+                ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
 
-               auto_install = true,
+                auto_install = true,
 
-               highlight = {
-                   enable = true,
-               },
-               incremental_selection = {
-                   enable = true,
-                   keymaps = {
-                       init_selection = "<leader>ss", -- set to `false` to disable one of the mappings
-                       node_incremental = "<leader>si",
-                       scope_incremental = "<leader>sc",
-                       node_decremental = "<leader>sd",
-                   },
-               },
+                highlight = {
+                    enable = true,
+                },
+                incremental_selection = {
+                    enable = true,
+                    keymaps = {
+                        init_selection = "<leader>ss", -- set to `false` to disable one of the mappings
+                        node_incremental = "<leader>si",
+                        scope_incremental = "<leader>sc",
+                        node_decremental = "<leader>sd",
+                    },
+                },
 
-               textobjects = {
-                   select = {
-                       enable = true,
+                textobjects = {
+                    select = {
+                        enable = true,
 
-                       -- Automatically jump forward to textobj, similar to targets.vim
-                       lookahead = true,
+                        -- Automatically jump forward to textobj, similar to targets.vim
+                        lookahead = true,
 
-                       keymaps = {
-                           -- You can use the capture groups defined in textobjects.scm
-                           ["af"] = "@function.outer",
-                           ["if"] = "@function.inner",
-                           ["ac"] = "@class.outer",
-                           -- You can optionally set descriptions to the mappings (used in the desc parameter of
-                           ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-                           -- You can also use captures from other query groups like `locals.scm`
-                           ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
-                       },
-                       selection_modes = {
-                           ['@parameter.outer'] = 'v', -- charwise
-                           ['@function.outer'] = 'V', -- linewise
-                           ['@class.outer'] = '<c-v>', -- blockwise
-                       },
-                       include_surrounding_whitespace = true,
-                   },
-               },
-           })
-       end,
-   },
-   {
-       "nvim-treesitter/nvim-treesitter-textobjects",
-   },
-   {
-       "neovim/nvim-lspconfig",
-   },
-   {
-       "williamboman/mason.nvim",
-       config = function()
-           require("mason").setup()
-       end
-   },
-   {
-       "williamboman/mason-lspconfig.nvim",
-       dependencies = { "mason.nvim" },
-       config = function()
-           require("mason-lspconfig").setup()
-           require("mason-lspconfig").setup_handlers({
-            function(server_name)
-                require("lspconfig")[server_name].setup({})
-            end,
-           })
-       end
-   }
+                        keymaps = {
+                            -- You can use the capture groups defined in textobjects.scm
+                            ["af"] = "@function.outer",
+                            ["if"] = "@function.inner",
+                            ["ac"] = "@class.outer",
+                            -- You can optionally set descriptions to the mappings (used in the desc parameter of
+                            ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
+                            -- You can also use captures from other query groups like `locals.scm`
+                            ["as"] = { query = "@scope", query_group = "locals", desc = "Select language scope" },
+                        },
+                        selection_modes = {
+                            ['@parameter.outer'] = 'v', -- charwise
+                            ['@function.outer'] = 'V',  -- linewise
+                            ['@class.outer'] = '<c-v>', -- blockwise
+                        },
+                        include_surrounding_whitespace = true,
+                    },
+                },
+            })
+        end,
+    },
+    {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    {
+        "neovim/nvim-lspconfig",
+    },
+    {
+        "williamboman/mason.nvim",
+        config = function()
+            require("mason").setup()
+        end
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = { "mason.nvim" },
+        config = function()
+            require("mason-lspconfig").setup()
+            require("mason-lspconfig").setup_handlers({
+                function(server_name)
+                    require("lspconfig")[server_name].setup({})
+                end,
+            })
+        end
+    },
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.6',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        config = function()
+            local builtin = require('telescope.builtin')
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+            vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+            vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+        end,
+    },
+    {
+        'stevearc/oil.nvim',
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        keys = {
+            {
+                '<leader>o',
+                function()
+                    require('oil').open()
+                end,
+                desc = '[F]ormat buffer',
+            }
+        },
+        opts = {
+            default_file_explorer = true,
+            view_options = {
+                show_hidden = true,
+            },
+        },
+    },
+    {
+        "ThePrimeagen/harpoon",
+        branch = "harpoon2",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local harpoon = require("harpoon")
+
+            -- REQUIRED
+            harpoon:setup()
+            -- REQUIRED
+
+            vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+            vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+
+            vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
+            vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
+            vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
+            vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+
+            -- Toggle previous & next buffers stored within Harpoon list
+            vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+            vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+        end
+    },
+    {
+        "mfussenegger/nvim-lint",
+        config = function()
+            local lint = require("lint")
+
+            lint.linters_by_ft = {
+                javascript = { "eslint_d" },
+                typescript = { "eslint_d" },
+                javascriptreact = { "eslint_d" },
+                typescriptreact = { "eslint_d" },
+                svelte = { "eslint_d" },
+            }
+
+            local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+            vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+                group = lint_augroup,
+                callback = function()
+                    lint.try_lint()
+                end,
+            })
+
+            vim.keymap.set("n", "<leader>ll", function()
+                lint.try_lint()
+            end, { desc = "Trigger linting for current file" })
+        end,
+    },
+    {
+        "stevearc/conform.nvim",
+        config = function()
+            local conform = require("conform")
+
+            conform.setup({
+                formatters_by_ft = {
+                    lua = { "stylua" },
+                    svelte = { { "prettierd", "prettier" } },
+                    javascript = { { "prettierd", "prettier" } },
+                    typescript = { { "prettierd", "prettier" } },
+                    javascriptreact = { { "prettierd", "prettier" } },
+                    typescriptreact = { { "prettierd", "prettier" } },
+                    json = { { "prettierd", "prettier" } },
+                    graphql = { { "prettierd", "prettier" } },
+                    java = { "google-java-format" },
+                    markdown = { { "prettierd", "prettier" } },
+                    html = { "htmlbeautifier" },
+                    bash = { "beautysh" },
+                    proto = { "buf" },
+                    toml = { "taplo" },
+                    css = { { "prettierd", "prettier" } },
+                    scss = { { "prettierd", "prettier" } },
+                    sh = { { "shellcheck" } },
+                },
+            })
+
+            vim.keymap.set({ "n", "v" }, "<leader>p", function()
+                conform.format({
+                    lsp_fallback = true,
+                    async = false,
+                    timeout_ms = 1000,
+                })
+            end, { desc = "Format file or range (in visual mode)" })
+        end,
+    },
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = true
+        -- use opts = {} for passing setup options
+        -- this is equalent to setup({}) function
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        dependencies = {
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-nvim-lsp",
+            "L3MON4D3/LuaSnip",
+            "saadparwaiz1/cmp_luasnip",
+            "rafamadriz/friendly-snippets",
+            "onsails/lspkind.nvim"
+        },
+        config = function()
+            local cmp = require("cmp")
+
+            local luasnip = require("luasnip")
+
+            local lspkind = require("lspkind")
+
+            require("luasnip.loaders.from_vscode").lazy_load()
+
+            cmp.setup({
+                sources = {
+                    { name = "nvim_lsp" },
+                    { name = "buffer" },
+                    { name = "path" },
+                    { name = "luasnip" },
+                },
+                completion = {
+                    completeopt = "menu,menuone,preview,noselect",
+                },
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-p]"] = cmp.mapping.select_prev_item(),
+                    ["<C-n]"] = cmp.mapping.select_next_item(),
+                    ["<C-u]"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-d]"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-e>"] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+                    pl
+                }),
+
+                formatting = {
+                    format = lspkind.cmp_format({
+                        maxwidth = 50,
+                        ellipsis_char = "...",
+                    })
+                }
+            })
+        end
+    }
 })
